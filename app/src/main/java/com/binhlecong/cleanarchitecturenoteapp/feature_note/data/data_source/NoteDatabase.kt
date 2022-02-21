3c15fd11fc8b6dd5e9263ffecacfdc8bf7314d72
binhlecong <congbinh21012001@gmail.com>
2022-02-21 07:39:10 +0700

    feat - Dagger-hilt setup

--- app/src/main/java/com/binhlecong/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/binhlecong/cleanarchitecturenoteapp/feature_note/data/data_source/NoteDatabase.kt
@@ -2,12 +2,18 @@ package com.binhlecong.cleanarchitecturenoteapp.feature_note.data.data_source
 
 import androidx.room.Database
 import androidx.room.Entity
+import androidx.room.RoomDatabase
 import com.binhlecong.cleanarchitecturenoteapp.feature_note.domain.model.Note
 
 @Database(
     entities = [Note::class],
     version = 1
 )
-abstract class NoteDatabase {
+abstract class NoteDatabase : RoomDatabase() {
+
     abstract val noteDao: NoteDao
+
+    companion object {
+        const val DATABASE_NAME = "notes_db"
+    }
 }
